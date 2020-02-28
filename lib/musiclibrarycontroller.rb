4b458eb8c0e39627c0fbdd051d3c2606ed029188
Lester Young <lyoung070789@gmail.com>
2020-02-28 02:50:59 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -58,7 +58,7 @@ class MusicLibraryController
     input = gets.chomp
     if genre = Genre.find_by_name(input)
       genre.songs.sort{|a,b| a.name<=>b.name}.each_with_index do |i, j|
-        puts "#{j+1}. "
+        puts "#{j+1}. #{i.artist.name} - #{i.song.name}"
       end 
     end 
   end 
