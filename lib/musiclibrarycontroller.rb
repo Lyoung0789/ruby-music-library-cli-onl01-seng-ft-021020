6f5c2abdf41dc0c0b4d10c5daa75d80e84e5eb29
Lester Young <lyoung070789@gmail.com>
2020-02-28 02:31:54 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -33,7 +33,7 @@ class MusicLibraryController
   def list_artists
     Artist.all.sort{|artist_a, artist_b| artist_a.name <=> artist_b.name}.each do |i, j|
       # binding.pry
-      puts "#{j+1}. #{i.name}"
+      puts "#{j+1}."
     end 
   end 
 
